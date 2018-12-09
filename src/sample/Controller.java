5dc1ea800f6193f87b9dc1017387d780fb109a5d
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-12-09 16:19:15 +0100

    FXML + Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,9 @@
 package sample;
 
-//import algorithm.Algorithm;
-//import data.SaveToFile;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-//import keys.GenerateKey;
 import methods.Conversions;
 
 import javax.swing.*;
@@ -15,22 +11,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Controller {
-    private List<Byte> encoded;
-    private List<Byte> decoded;
-   // private Algorithm algorithm;
     private File file;
-   // private SaveToFile saveToFile;
-   // private GenerateKey generateKey;
-    private String end;
 
     @FXML
-    public TextArea key, messages, selectedFile;
+    public TextArea privateKey, publicKey, selectedFile, messages;
     @FXML
-    public Button browse, encode, decode, randomKey;
+    public Button browse, verifySignature, generateSignature;
 
     public Controller() {
         /*algorithm = new Algorithm();
@@ -38,26 +27,18 @@ public class Controller {
         generateKey = new GenerateKey();*/
     }
 
-    public void pressBrowse(ActionEvent event) {
-
+    public void pressBrowse() {
         JFileChooser fc = new JFileChooser(new File("").getAbsolutePath());
         int returnVal = fc.showOpenDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             file = fc.getSelectedFile();
             selectedFile.setText("Selected File: " + file.getPath());
             messages.setText("File succesfuly loaded.");
-            String s = file.getName();
-            char[] pom = s.toCharArray();
-            int pom2 = pom.length-1;
-            while (pom[pom2] != '.') {
-                pom2--;
-            }
-            end = s.substring(pom2);
         }
     }
 
-    public void pressEncode(ActionEvent event) {
-        if(file != null && key.getText().length() == 48) {
+    public void pressVerifySignature(ActionEvent event) {
+        if(file != null && publicKey.getText().length() > 0) {
             /*encode.setDisable(true);
             encoded = algorithm.encode3DES(getBytesFromFile(), key.getText());
             saveToFile.save("encode" + end, encoded, 0);
@@ -66,8 +47,8 @@ public class Controller {
         }
     }
 
-    public void pressDecode(ActionEvent event) {
-        if(file != null && key.getText().length() == 48) {
+    public void pressGenerateSignature(ActionEvent event) {
+        if(file != null && privateKey.getText().length() > 0) {
             /*decode.setDisable(true);
             decoded = algorithm.decode3DES(getBytesFromFile(), key.getText());
             saveToFile.save("decode" + end, decoded, 0);
@@ -88,9 +69,5 @@ public class Controller {
             e.printStackTrace();
         }
         return list;
-    }
-
-    public void pressRandomKey(ActionEvent event) {
-        //key.setText(generateKey.getRandomKey());
     }
 }
