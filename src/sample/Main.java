05ea9bb31183007b56937098a8c87f99589f9ad3
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-12-09 19:23:52 +0100

    Should work

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,15 @@
 package sample;
 
-import algorithm.DSA;
-import algorithm.Sha1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import methods.ExponentiationBySquaring;
-
-import java.math.BigInteger;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 500));
@@ -23,15 +18,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("dupa");
-        String dupa = "wziąść";
-        Sha1 sha1 = new Sha1();
-        String ans = sha1.hasz(dupa);
-        System.out.println(ans);
-
-        DSA dsa = new DSA();
-        dsa.generateNumbers();
-        System.out.println("dupa");
         launch(args);
     }
 }
