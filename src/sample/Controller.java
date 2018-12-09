8e847bad087d5dcc5d33489350fbeec73c8bae1f
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-12-09 16:41:49 +0100

    Simplified Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,12 @@
 package sample;
 
 import algorithm.Algorithm;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import methods.Conversions;
 
 import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
     private File file;
@@ -40,9 +34,9 @@ public class Controller {
     }
 
     public void pressVerifyFile() {
-        if(file != null && publicKey.getText().length() > 0) {
+        if (file != null && publicKey.getText().length() > 0) {
             boolean isVerified = algorithm.verifyFile(publicKey.getText(), file);
-            if(isVerified) {
+            if (isVerified) {
                 messages.setText("File is correct.");
             } else {
                 messages.setText("File is incorrect.");
@@ -51,23 +45,9 @@ public class Controller {
     }
 
     public void pressGenerateSignature() {
-        if(file != null && privateKey.getText().length() > 0) {
+        if (file != null && privateKey.getText().length() > 0) {
             publicKey.setText(algorithm.generateKey(privateKey.getText(), file));
             messages.setText("Public key has been generated.");
         }
-    }
-
-    private List<Byte> getBytesFromFile() {
-        List<Byte> list = new ArrayList<>();
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            for(Byte b : bytes) {
-                List<Byte> pom = Conversions.numberTo8Byte(b);
-                list.addAll(pom);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;
     }
 }
