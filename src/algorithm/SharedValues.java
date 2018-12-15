f7b1b3ec7d4043b9dccc70a36a10571192edcd82
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-12-15 17:09:30 +0100

    Class for shared values

---
+++ src/algorithm/SharedValues.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+import java.math.BigInteger;
+
+public class SharedValues {
+    public static final BigInteger p = new BigInteger("0");
+    public static final BigInteger q = new BigInteger("0");
+    public static final BigInteger g = new BigInteger("0");
+}
